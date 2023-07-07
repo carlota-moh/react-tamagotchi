cbee792489274b1568ea546f5f2b7bdb18829aad
Charly <cmonedero@stackbuilders.com>
2023-07-07 10:59:26 +0200

    Added react-refresh plugin

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,12 +1,18 @@
 /* dev configuration */
 const webpack = require('webpack')
+const ReactRefresgWbpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
     mode: 'development',
+    devServer : {
+        hot: true,
+        open: true
+    },
     devtool: 'cheap-module-source-map', /* for generating source maps */
     plugins : [
         new webpack.DefinePlugin({
             'process.env.name': JSON.stringify('cmonedero-dev')
-        })
+        }),
+        new ReactRefresgWbpackPlugin()
     ]
 }
