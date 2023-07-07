bcf63888190f3d94a8f6b28a79771e3cb9be4a1f
Charly <cmonedero@stackbuilders.com>
2023-07-07 10:21:17 +0200

    Configuration for rendering img

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import './styles.css'
+import IMAGE from './tamagotchi.png'
+import LOGO from './tamagotchi2.svg'
+
 export const App = () => {
-    return <h1>React Tamagotchi</h1>
+    return <>
+        <h1>React Tamagotchi</h1>
+        <img src={ IMAGE } alt="Tamagotchi image" width="200" height="200"/>
+        <img src={ LOGO } alt="Tamagotchi image" width="200" height="200"/>
+    </>
 }
