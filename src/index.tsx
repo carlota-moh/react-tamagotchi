3ee8d37dfd2b511072f00fe96eaa299f02313c3b
Charly <cmonedero@stackbuilders.com>
2023-07-12 17:51:55 +0200

    Basic routing structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,22 @@
 // imports
-import ReactDOM from 'react-dom'
-import { App } from './App'
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { App } from "./App";
+import ErrorPage from "./ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  }
+]);
 
 // mount App component into index.html
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+  document.getElementById("root")
+);
