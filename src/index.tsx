179a4747cce22f5523340ba87545daa4cc70a37c
Charly <cmonedero@stackbuilders.com>
2023-07-19 10:11:25 +0200

    Initial router configuration

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,11 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import ErrorPage from "./ErrorPage";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import Hello from "./hello";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const router = createBrowserRouter([
   {
@@ -15,12 +16,13 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <ErrorPage />,
   },
+  {
+    path: "hello",
+    element: <Hello />,
+    errorElement: <ErrorPage />,
+  },
 ]);
 
-const container = document.getElementById("root");
-const root = createRoot(container!);
-
-root.render(
-    <App />
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <RouterProvider router={router} />
 );
-
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -35,6 +35,10 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '..', './build'), 
     filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   plugins: [
     /* insert the plugin into the index.html */
