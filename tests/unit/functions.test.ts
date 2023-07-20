71099763b536dc0c1eee6ca2aa2ee45ca42a1235
Charly <cmonedero@stackbuilders.com>
2023-07-20 11:02:25 +0200

    Configured MSW

--- tests/unit/functions.test.ts
+++ tests/unit/functions.test.ts
@@ -1,9 +1,28 @@
-import { parsePokemon } from "../../src/components/areas/functions";
-import data from "./data.json";
+import {
+  fetchPokemon,
+  parsePokemon,
+} from "../../src/components/areas/functions";
+import data from './data.json'
 
 describe("API behavior", () => {
-  it("Returns a formatted pokemon", () => {
-    const pokemonData = parsePokemon(data);
+  it("Correctly formats a Pokémon", () => {
+    const pokemonData = parsePokemon(data)
+
+    expect(pokemonData).toEqual({
+      name: "ditto",
+      types: [
+        {
+          slot: 1,
+          type: { name: "normal", url: "https://pokeapi.co/api/v2/type/1/" },
+        },
+      ],
+      weight: 40,
+      img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
+    });
+  });
+
+  it("Returns a formatted Pokémon", async () => {
+    const pokemonData = await fetchPokemon("dummy-pokemon");
 
     expect(pokemonData).toEqual({
       name: "ditto",
