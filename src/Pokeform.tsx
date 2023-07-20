5c642d16c0d71e2e7c82ccb7153810b44637beb8
Charly <cmonedero@stackbuilders.com>
2023-07-20 15:22:12 +0200

    Form now modifying global state

--- src/Pokeform.tsx
+++ src/Pokeform.tsx
@@ -1,5 +1,8 @@
 import { SubmitHandler, useForm } from "react-hook-form";
+import { useContext } from "react";
+import { pokemonDataContext, pokemonHappinessContext } from "./App";
 import { fetchPokemon } from "./components/areas/functions";
+
 
 interface PokeFormInput {
   pokemonName: string;
@@ -7,13 +10,17 @@ interface PokeFormInput {
 
 const PokeFormPage = () => {
   const { register, handleSubmit } = useForm<PokeFormInput>();
+  const { pokemonData, dispatchData } = useContext(pokemonDataContext);
+  const { pokemonHappiness, dispatchHappy } = useContext(
+    pokemonHappinessContext
+  );
 
   const onSubmit: SubmitHandler<PokeFormInput> = async (data) => {
-    // dispatchHappy({ type: "new" });
+    dispatchHappy({ type: "new" });
     const pokemonData = await fetchPokemon(data.pokemonName).catch(
       (e) => alert("Invalid pokemon")
     );
-    // dispatchData({ type: "new", pokemonData: pokemonData });
+    dispatchData({ type: "new", pokemonData: pokemonData });
     console.log(pokemonData);
   };
 
