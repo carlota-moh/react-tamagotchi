727f6bf4dafc90493c47aaa60c7cf39d4c22c32f
Charly <cmonedero@stackbuilders.com>
2023-07-20 11:39:13 +0200

    Unified test location

--- tests/unit/buttons.test.tsx
+++ tests/unit/buttons.test.tsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import App from "../../App";
+import App from "../../src/App";
 
 describe("Buttons render correctly", () => {
   beforeEach(() => {
